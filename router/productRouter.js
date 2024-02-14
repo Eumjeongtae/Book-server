236fb8a5ced8fbb2c0862b2197f87fbff98ab6e8
eumjeongtae <dmawjdxo12@naver.com>
2024-02-14 18:04:06 +0900

    add img

--- router/productRouter.js
+++ router/productRouter.js
@@ -4,5 +4,7 @@ import * as productController from "../controller/productController.js";
 const router = express.Router();
 
 router.get('/', productController.getList);
+router.get('/:bid', productController.getDetial);
+
 
 export default router;
