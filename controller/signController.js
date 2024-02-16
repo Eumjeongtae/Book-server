e758dc94151d192c4cd2265467e4a0f2716e8848
eumjeongtae <dmawjdxo12@naver.com>
2024-02-16 16:44:32 +0900

    feat 메일 인증코드

--- controller/signController.js
+++ controller/signController.js
@@ -11,24 +11,38 @@ const transporter = nodemailer.createTransport({
 		pass: process.env.NAVER_PASS,  // 네이버 비밀번호
 	},
 });
+function generateRandomNumbers(min, max, count) {
+    let numbers = [];
+
+    while (numbers.length < count) {
+        let n = Math.floor(Math.random() * (max - min + 1)) + min;
+        if (!numbers.includes(n)) {
+            numbers.push(n);
+        }
+    }
+
+    return numbers.join('');
+}
+
+
 
 export async function emailCheck(req, res) {
-    let { email, mail2 } = req.body;
-    const mail = email + mail2;
-
+    let {data} = req.body
+    let mail = data
+    const randomNumbers = generateRandomNumbers(1, 9, 6);
     const mailOptions = {
         from: process.env.NAVER_ID,
         to: mail,
         subject: "인증 관련 메일 입니다.",
-        html: '<h1>인증번호를 입력해주세요 41112</h1>'
+        html: `<h1>인증번호를 입력해주세요 ${randomNumbers}</h1>`
     };
 
     try {
         const response = await transporter.sendMail(mailOptions);
-        console.log("Mail Sent Response:", response);
-        res.json({ ok: true, msg: '메일 전송에 성공하였습니다.', authNum: 41112 });
+        // console.log("Mail Sent Response:", response);
+        res.json({ ok: true, msg: '메일 전송에 성공하였습니다.', authNum: randomNumbers });
     } catch (err) {
-        console.error("Send Mail Error:", err);
+        // console.error("Send Mail Error:", err);
         res.json({ ok: false, msg: '메일 전송에 실패하였습니다.' });
     } finally {
         transporter.close(); // 전송 종료
--- router/signupRouter.js
+++ router/signupRouter.js
@@ -4,5 +4,6 @@ import * as signController from "../controller/signController.js";
 const router = express.Router();
 
 router.post('/', signController.emailCheck);
+router.post('/mailCheck', signController.emailCheck);
 
 export default router;
